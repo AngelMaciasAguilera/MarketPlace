c4dc729707fe697919b06ff348340c2fc604d66d
Angel <maciasaguilerajoseangel@gmail.com>
2024-01-08 13:29:49 +0100

    Seccond commit

---
+++ app/src/main/java/com/example/marketplace/vistas/SingInScreen.java
@@ -0,0 +1,16 @@
+package com.example.marketplace.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.marketplace.R;
+
+public class SingInScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_in_screen);
+    }
+}
