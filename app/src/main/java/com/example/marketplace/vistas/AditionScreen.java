827ea32a7b2f50811fb3d7e97881b3fd41b0991a
Angel <maciasaguilerajoseangel@gmail.com>
2024-01-23 19:19:57 +0100

    Fourth Commit

---
+++ app/src/main/java/com/example/marketplace/vistas/AditionScreen.java
@@ -0,0 +1,16 @@
+package com.example.marketplace.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.marketplace.R;
+
+public class AditionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adition_screen);
+    }
+}
